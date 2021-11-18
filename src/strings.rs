52d0e230663f62fd64fbf903148298ce0c126bb8
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-11-18 10:41:39 +0100

    jlk

--- src/strings.rs
+++ src/strings.rs
@@ -3,8 +3,44 @@
 
 
 pub fn run() {
-    let hello = String::from("hello");
+    let mut hello = String::from("hello ");
 
-    println!("{}", hello);
+ // get length
+    println!("Length: {}", hello.len());
+
+// pust char
+    hello.push('w');
+
+// pust string
+    hello.push_str("orld!");
+
+//capacity in bytes
+    println!("Capacity: {}", hello.capacity());
+
+// chack if empty
+    println!("is empty: {}", hello.is_empty());
+
+//contains
+println!("contains 'world' {}", hello.contains("world"));
+
+//replace
+println!("replace: {}", hello.replace("world", "there"));
+
+//loop through string by whitespace
+for word in hello.split_whitespace() {
+    println!("{}", word);
+}
+
+
+//create string with capacity
+let mut s = String::with_capacity(10);
+s.push('a');    
+s.push('b');
+
+//assertion testing
+assert_eq!(3, s.len());
+
+    println!("{}", s);
+
 
 }
