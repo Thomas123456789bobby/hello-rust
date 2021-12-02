55dd3022985282aa19d45c7ca006c215957f30a4
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-12-02 08:34:31 +0100

    ya

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-//mod print;
-//mod vars;
-//mod types;
-//mod strings;
-// mod tuples;
+mod print;
+mod vars;
+mod types;
+mod strings;
+mod tuples;
 mod arrays;
+mod button;
+
 
 fn main() {
-    //print::run();
-    //vars::run();
-    //types::run();
-    //strings::run();
-    // tuples::run();  
+    print::run();
+    vars::run();
+    types::run();
+    strings::run();
+    tuples::run();  
     arrays::run();  
+    button::run();  
+}
+let n:i32 = 0;
+while n <1000000 {
+    n++;
 }
  
